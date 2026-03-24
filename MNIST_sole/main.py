93031612395fd0648a12f76fa2a85d88c6e9bcca
Miyawa <rx.xie@stu.xjtu.edu.cn>
2026-03-24 20:07:28 +0800

    main and model

---
+++ MNIST_sole/main.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from data_setup import get_mnist_loaders
+from model import SimpleNet
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+
+    train_loader, test_loader = get_mnist_loaders(batch_size=64)
+    model = SimpleNet().to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    epochs = 3 
+
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        
+        for batch_idx, (images, labels) in enumerate(train_loader):
+            images, labels = images.to(device), labels.to(device)
+            
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+            if batch_idx % 100 == 99:  
+                print(f'Epoch [{epoch+1}/{epochs}], Step [{batch_idx+1}/{len(train_loader)}], Loss: {running_loss/100:.4f}')
+                running_loss = 0.0
+
+        model.eval()
+        correct = 0
+        total = 0
+        
+        with torch.no_grad():
+            for batch_idx, (images, labels) in enumerate(test_loader):
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                _, predicted =  torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+
+        print(f'---> Epoch {epoch+1} Test Accuracy: {100 * correct / total:.2f}%\n')
+
+if __name__ == '__main__':
+    main()
---
+++ MNIST_sole/model.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super().__init__() 
+        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
+        self.pool = nn.MaxPool2d(2)
+        self.fc = nn.Linear(128*7*7,10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+
+        x = x.view(-1,128*7*7)
+        x = self.fc(x)
+        return x
